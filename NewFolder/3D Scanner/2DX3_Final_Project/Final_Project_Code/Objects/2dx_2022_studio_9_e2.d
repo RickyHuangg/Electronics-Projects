84db51e3cf34a27200975bebc1c47ac7eafbcd8f
RickyHuangg <124844009+RickyHuangg@users.noreply.github.com>
2025-06-18 15:01:09 -0400

    Delete NewFolder directory

--- NewFolder/3D Scanner/2DX3_Final_Project/Final_Project_Code/Objects/2dx_2022_studio_9_e2.d
+++
@@ -1,7 +0,0 @@
-./objects/2dx_2022_studio_9_e2.o: 2DX_2022_Studio_9_E2.c \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h PLL.h SysTick.h uart.h \
-  onboardLEDs.h tm4c1294ncpdt.h VL53L1X_api.h vl53l1_platform.h \
-  vl53l1_types.h C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\string.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h
